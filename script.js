000fa12c54b56f9cf542a3c353914398d7523832
SayedKhalilRahman-ai <skhalilhussaini68@gmail.com>
2026-09-13 14:31:05 +0430

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const theme=document.getElementById("theme");
+
+theme.onclick=()=>{
+document.body.classList.toggle("light");
+};
+
+document.getElementById("mobile").onclick=()=>{
+document.getElementById("menu").style.display="flex";
+};
